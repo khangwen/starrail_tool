c984946f4347196135a00f07dfc7b9328fc82aa2
Alan Nguyen <a_wen197@yahoo.com>
2023-09-28 18:37:21 -0700

    added new pages, changed images, naming, menu

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -4,60 +4,36 @@ type IMenuDataType = {
   title: string;
   link: string;
   sub_menu?: {
-      title: string;
-      link: string;
+    title: string;
+    link: string;
   }[];
 }
 
-const menu_data:IMenuDataType[] = [
+const menu_data: IMenuDataType[] = [
   {
-    id:1,
-    title:'Home',
-    link:'#',
-    sub_menu:[
-      {title:'Home One',link:'/'},
-      {title:'Home Two',link:'/home-2'},
-    ]
+    id: 1,
+    title: 'Home',
+    link: '/',
   },
   {
-    id:2,
-    title:'ABOUT US',
-    link:'/about',
+    id: 2,
+    title: 'ABOUT US',
+    link: '/about',
   },
   {
-    id:3,
-    title:'TOURNAMENT',
-    link:'#',
-    sub_menu:[
-      {title:'TOURNAMENT',link:'/tournament'},
-      {title:'TOURNAMENT Single',link:'/tournament-details'},
-    ]
+    id: 3,
+    title: 'CALCULATOR',
+    link: '/calculator',
   },
   {
-    id:4,
-    title:'PAGES',
-    link:'#',
-    sub_menu:[
-      {title:'Gaming Shop',link:'/shop'},
-      {title:'Shop Details',link:'/shop-details'},
-      {title:'Our Services',link:'/services'},
-      {title:'Services Details',link:'/service-details'},
-      {title:'Player Details',link:'/team-details'},
-    ]
+    id: 4,
+    title: 'Simulator',
+    link: '/simulator',
   },
   {
-    id:5,
-    title:'News',
-    link:'#',
-    sub_menu:[
-      {title:'Our Blog',link:'/blog'},
-      {title:'Blog Details',link:'/blog-details'}
-    ]
-  },
-  {
-    id:6,
-    title:'CONTACT',
-    link:'/contact',
+    id: 5,
+    title: 'Relic Optimizer',
+    link: '/relic-optimizer',
   },
 ]
 
